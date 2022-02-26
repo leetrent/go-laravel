64bfde051c23b404d532c4dcdb2a82f1cbd22167
LeeTrent <lee.trent.1@gmail.com>
2022-02-25 21:15:33 -0600

    Now using test tables

--- celeritas/render/render_test.go
+++ celeritas/render/render_test.go
@@ -6,93 +6,39 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-go-file", true, "no error when rendering a non-existent go template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-jet-file", true, "no error when rendering a non-existent jet template, when one is expected"},
+	{"invalid-rendering-engine", "foo", "home", true, "no error when non-existent template rendering engine was encountered (error was expected)"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		w := httptest.NewRecorder()
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
 
-	w := httptest.NewRecorder()
-
-	testRenderer.RootPath = "./testdata"
-
-	////////////////////////////////////////////////
-	// TEST GO TEMPLATES
-	////////////////////////////////////////////////
-	testRenderer.Renderer = "go"
-	// GO TEMPLATE FILE WAS FOUND
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering Go template page", err)
-	}
-	// GO TEMPLATE DOESN'T EXIST
-	err = testRenderer.Page(w, r, "no-go-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent Go template", err)
-	}
-
-	////////////////////////////////////////////////
-	// TEST JET TEMPLATES
-	////////////////////////////////////////////////
-	testRenderer.Renderer = "jet"
-	// JET TEMPLATE FILE WAS FOUND
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering Jet template page", err)
-	}
-	// JET TEMPLATE DOESN'T EXIST
-	err = testRenderer.Page(w, r, "no-jet-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent Jet template", err)
-	}
-
-	////////////////////////////////////////////////
-	// NO TEMPLATE ENGINE SPECIFIED
-	////////////////////////////////////////////////
-	testRenderer.Renderer = ""
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No error returned when template engine is not specified", err)
-	}
-
-}
-
-func TestRender_GoPage(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
-
-	testRenderer.Renderer = "go"
-	// GO TEMPLATE FILE WAS FOUND
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering Go template page", err)
-	}
-	// GO TEMPLATE DOESN'T EXIST
-	err = testRenderer.Page(w, r, "no-go-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent Go template", err)
-	}
-}
-
-func TestRender_JetPage(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
-
-	testRenderer.Renderer = "jet"
-	// JET TEMPLATE FILE WAS FOUND
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering Jet template page", err)
-	}
-	// GO TEMPLATE DOESN'T EXIST
-	err = testRenderer.Page(w, r, "no-jet-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent Jet template", err)
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s %s", e.name, e.errorMessage, err.Error())
+			}
+		}
 	}
 }
