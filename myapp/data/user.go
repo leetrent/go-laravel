5aa4ff8047a4c5ed2421868a388f9fcbcbdb8fda
LeeTrent <lee.trent.1@gmail.com>
2022-03-06 11:24:32 -0600

    Completed CRUD methods for User model

--- myapp/data/user.go
+++ myapp/data/user.go
@@ -1,8 +1,11 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 
 	up "github.com/upper/db/v4"
 )
@@ -41,7 +44,7 @@ func (u *User) GetAll() ([]*User, error) {
 func (u *User) GetByEmail(email string) (*User, error) {
 
 	////////////////////////////////////////////////
-	// USERS TABLES
+	// USERS TABLE
 	////////////////////////////////////////////////
 	var theUser User
 	collection := upper.Collection(u.Table())
@@ -53,7 +56,7 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	}
 
 	////////////////////////////////////////////////
-	// TOKENS TABLES
+	// TOKENS TABLE
 	////////////////////////////////////////////////
 	var token Token
 	collection = upper.Collection(token.Table())
@@ -65,7 +68,122 @@ func (u *User) GetByEmail(email string) (*User, error) {
 			return nil, err
 		}
 	}
-
 	theUser.Token = token
+
 	return &theUser, nil
+}
+
+func (u *User) Get(id int) (*User, error) {
+
+	////////////////////////////////////////////////
+	// USERS TABLE
+	////////////////////////////////////////////////
+	var theUser User
+	collection := upper.Collection(u.Table())
+	res := collection.Find(up.Cond{"id =": id})
+	err := res.One(&theUser)
+	if err != nil {
+		fmt.Print(err)
+		return nil, err
+	}
+
+	////////////////////////////////////////////////
+	// TOKENS TABLE
+	////////////////////////////////////////////////
+	var token Token
+	collection = upper.Collection(token.Table())
+	res = collection.Find(up.Cond{"user_id =": theUser.ID, "expiry <": time.Now()}).OrderBy("created_at desc")
+	err = res.One(&token)
+	if err != nil {
+		fmt.Print(err)
+		if err != up.ErrNilRecord && err != up.ErrNoMoreRows {
+			return nil, err
+		}
+	}
+	theUser.Token = token
+
+	return &theUser, nil
+}
+
+func (u *User) Update(theUser User) error {
+	theUser.UpdatedAt = time.Now()
+	collection := upper.Collection(u.Table())
+	res := collection.Find(theUser.ID)
+	err := res.Update(&theUser)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	return nil
+}
+
+func (u *User) Delete(id int) error {
+	collection := upper.Collection(u.Table())
+	res := collection.Find(id)
+	err := res.Delete()
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	return nil
+}
+
+func (u *User) Insert(theUser User) (int, error) {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(theUser.Password), 12)
+	if err != nil {
+		fmt.Print(err)
+		return 0, err
+	}
+
+	theUser.CreatedAt = time.Now()
+	theUser.UpdatedAt = time.Now()
+	theUser.Password = string(newHash)
+
+	collection := upper.Collection(u.Table())
+	res, err := collection.Insert(theUser)
+
+	if err != nil {
+		fmt.Print(err)
+		return 0, err
+	}
+
+	id := getInsertID(res.ID())
+	return id, nil
+}
+
+func (u *User) ResetPassword(id int, password string) error {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+
+	theUser, err := u.Get(id)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+
+	u.Password = string(newHash)
+
+	err = theUser.Update(*u)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) PasswordMatches(plainText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plainText))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
 }
