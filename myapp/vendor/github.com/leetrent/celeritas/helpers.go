e13ceff9015a87e5aabdd81e6ffa1436a1de9eb4
LeeTrent <lee.trent.1@gmail.com>
2022-03-07 13:08:35 -0600

    Continuing to test User CRUD functionality in web app

--- myapp/vendor/github.com/leetrent/celeritas/helpers.go
+++ myapp/vendor/github.com/leetrent/celeritas/helpers.go
@@ -1,6 +1,27 @@
 package celeritas
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+// RandomString generates a random string of length n
+// from the values in the const randomString
+func (c *Celeritas) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
 
 func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	const mode = 0755
