df79978adfa73e9adbf921a8bd370da95433a673
LeeTrent <lee.trent.1@gmail.com>
2022-03-16 20:59:31 -0500

    Added user login/logout functionality

--- myapp/routes.go
+++ myapp/routes.go
@@ -21,6 +21,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
+	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
+	a.App.Routes.Get("/users/logout", a.Handlers.UserLogout)
 
 	//////////////////////////////////////////
 	// TEST DATABASE
