fc474ba8517a0749b0bec3be897de6cd52b3bd2d
LeeTrent <lee.trent.1@gmail.com>
2022-03-21 10:38:49 -0500

    Just getting started on Celeritas CLI development

---
+++ celeritas/cmd/cli/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/leetrent/celeritas"
+)
+
+const version = "1.0.0"
+
+var cel celeritas.Celeritas
+
+func main() {
+	arg1, arg2, arg3, err := validateInput()
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	switch arg1 {
+	case "help":
+		showHelp()
+	case "version":
+		showVersion()
+	default:
+		log.Println(arg2, arg3)
+	}
+}
+
+func validateInput() (string, string, string, error) {
+	var arg1, arg2, arg3 string
+
+	if len(os.Args) <= 1 {
+		color.Red("Error: command required")
+		showHelp()
+		return "", "", "", errors.New("command required")
+	}
+
+	arg1 = os.Args[1]
+	if len(os.Args) >= 3 {
+		arg2 = os.Args[2]
+	}
+	if len(os.Args) >= 4 {
+		arg3 = os.Args[3]
+	}
+
+	return arg1, arg2, arg3, nil
+}
+
+func showHelp() {
+	color.Yellow(`Available commands:
+	help		- show the help commands
+	version		- print application version
+	`)
+}
+
+func showVersion() {
+	color.Yellow("Application version: " + version)
+}
+
+func exitGracefully(err error, msg ...string) {
+	message := ""
+
+	if len(msg) > 0 {
+		message = msg[0]
+	}
+
+	if err != nil {
+		color.Red("Error: %v\n", err)
+	}
+
+	if len(message) > 0 {
+		color.Yellow(message)
+	} else {
+		color.Green("Finished!")
+	}
+
+	os.Exit(0)
+}
--- celeritas/go.mod
+++ celeritas/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
 	github.com/CloudyKit/jet/v6 v6.1.0 // indirect
 	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
@@ -16,6 +17,9 @@ require (
 	github.com/jackc/pgtype v1.10.0 // indirect
 	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/text v0.3.6 // indirect
 )
