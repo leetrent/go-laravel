203f4c8278a4c400f86ebd5ac1489f0a2361cc2a
LeeTrent <lee.trent.1@gmail.com>
2022-03-23 21:12:04 -0500

    Generating model class

--- celeritas/cmd/cli/make.go
+++ celeritas/cmd/cli/make.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gertd/go-pluralize"
 	"github.com/iancoleman/strcase"
 )
 
@@ -59,6 +60,41 @@ func doMake(arg2, arg3 string) error {
 			exitGracefully(err)
 		}
 
+	case "model":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the model a name"))
+		}
+
+		data, err := templateFS.ReadFile("templates/data/model.go.txt")
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		model := string(data)
+		plur := pluralize.NewClient()
+
+		var modelName = arg3
+		var tableName = arg3
+
+		if plur.IsPlural(arg3) {
+			modelName = plur.Singular(arg3)
+			tableName = strings.ToLower(tableName)
+		} else {
+			tableName = strings.ToLower(plur.Plural(arg3))
+		}
+
+		fileName := cel.RootPath + "/data/" + strings.ToLower(modelName) + ".go"
+		if fileExists(fileName) {
+			exitGracefully(errors.New(fileName + " already exists"))
+		}
+
+		model = strings.ReplaceAll(model, "$MODELNAME$", strcase.ToCamel(modelName))
+		model = strings.ReplaceAll(model, "$TABLENAME$", tableName)
+
+		err = copyDataToFile([]byte(model), fileName)
+		if err != nil {
+			exitGracefully(err)
+		}
 	}
 
 	return nil
