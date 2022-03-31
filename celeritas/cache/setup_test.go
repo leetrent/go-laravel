e1e770d73de3dc169d499cd0340cecfd944ece81
LeeTrent <lee.trent.1@gmail.com>
2022-03-31 11:38:22 -0500

    Performed Badger Cache setup

--- celeritas/cache/setup_test.go
+++ celeritas/cache/setup_test.go
@@ -1,17 +1,23 @@
 package cache
 
 import (
+	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gomodule/redigo/redis"
 )
 
 var testRedisCache RedisCache
+var testBadgerCache BadgerCache
 
 func TestMain(m *testing.M) {
+	/////////////////////////////////////////////////////////////
+	// REDIS CACHE
+	/////////////////////////////////////////////////////////////
 	s, err := miniredis.Run()
 	if err != nil {
 		panic(err)
@@ -31,6 +37,28 @@ func TestMain(m *testing.M) {
 	testRedisCache.Prefix = "test-celeritias"
 
 	defer testRedisCache.Conn.Close()
+
+	/////////////////////////////////////////////////////////////
+	// BADGER CACHE
+	/////////////////////////////////////////////////////////////
+	_ = os.RemoveAll("./testdata/tmp/badger")
+	if _, err := os.Stat("./testdata/tmp"); os.IsNotExist(err) {
+		err := os.Mkdir("./testdata/tmp", 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = os.Mkdir("./testdata/tmp/badger", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := badger.Open(badger.DefaultOptions("./testdata/tmp/badger"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testBadgerCache.Conn = db
 
 	os.Exit(m.Run())
 }
